55146f15aa5234b826f92cd1257e08548dee3469
Ignacio Jara <ijara756@gmail.com>
2022-06-21 18:43:18 -0400

    vista de todos los productos en lista

--- src/main/java/ijara/dev/ramito/controller/ProductoController.java
+++ src/main/java/ijara/dev/ramito/controller/ProductoController.java
@@ -1,15 +1,22 @@
 package ijara.dev.ramito.controller;
 
+import ijara.dev.ramito.model.Producto;
+import ijara.dev.ramito.repo.RepoProducto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class ProductoController {
+    @Autowired
+    RepoProducto repoProducto;
     @GetMapping("/productos")
-    public String listadoProductos(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String listadoProductos( Model model ) {
+        model.addAttribute("listadoProductos", repoProducto.findAll());
         return "productos";
     }
 }
